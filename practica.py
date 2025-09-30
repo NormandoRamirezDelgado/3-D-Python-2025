0b4054e387ad9918ccb54e3f93fa9d3f30ca6b97
Normando Ramírez <nramirezd@cbtis236.edu.mx>
2025-09-30 17:12:01 -0600

    .

---
+++ practica.py
@@ -0,0 +1,16 @@
+'''
+for i in range(11):
+    for j in range(0, i + 1):
+        print(j)
+    for j in range(i, -1, -1):
+        print(j)
+
+        '''
+
+for k in range(2):
+    print(0)
+for n in range(2, 11):
+    for i in range(n):
+        print(i)
+    for i in range(n-1, -1, -1):
+        print(i)
